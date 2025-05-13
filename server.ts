c2e2d646d0be8f8d7769f9ac4556a7a9a9f89159
Fredrick <fredabisai@gmail.com>
2025-05-13 21:59:16 +0300

    Initilize express app

--- server.ts
+++ server.ts
@@ -1,1 +1,8 @@
-import * as express from 'express';
+import  express from 'express';
+ const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
